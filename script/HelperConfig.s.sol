535660b5dee40125e0d5b2e7d1accb67f755017e
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-11-24 22:22:43 +0330

    Refactoring the mock smart contract pt.2

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -6,7 +6,7 @@ import {MockV3Aggregator} from '../test/mock/MockV3Aggregator.sol';
 
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
-    
+
     uint8 public constant DECIMALS = 8;
     int256 public constant INITIAL_PRICE = 2000e8;
 
@@ -16,7 +16,7 @@ contract HelperConfig is Script {
 
     constructor() {
         if(block.chainid == 11155111) activeNetworkConfig = getSepoliaEthConfig();
-        else activeNetworkConfig = getAnvilEthConfig();
+        else activeNetworkConfig = getOrCreateAnvilEthConfig();
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
@@ -26,7 +26,10 @@ contract HelperConfig is Script {
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if(activeNetworkConfig.priceFeed != address(0))
+            return activeNetworkConfig;
+
         vm.startBroadcast();
         MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
         vm.stopBroadcast();
