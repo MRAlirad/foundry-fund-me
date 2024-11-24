1c33cc015396f7de68cbea84c3adf8a17fa93beb
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-11-24 22:52:25 +0330

    Foundry tests cheatcodes

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -9,10 +9,11 @@ error NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public constant MINIMUM_USD = 5 * 1e18;
-    address[] public funders;
-    mapping (address => uint256) public addressToAmountFunded;
+    mapping (address => uint256) private s_addressToAmountFunded;
+    address[] private s_funders;
+
     address public i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 1e18;
     AggregatorV3Interface private s_priceFeed;
 
     constructor(address priceFeed){
@@ -23,17 +24,17 @@ contract FundMe {
     function fund() public payable {
         require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "Didn't send enough");
 
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = msg.value;
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] = msg.value;
     }
 
     function withdraw() public onlyOwner { 
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for(uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
 
-        funders = new address[](0);
+        s_funders = new address[](0);
 
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, 'Call Fails');
@@ -54,5 +55,13 @@ contract FundMe {
 
     fallback() external payable {
         fund();
+    }
+
+    function getAddressToAmountFunded(address fundingAddress) external view returns (uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) external view returns (address) {
+        return s_funders[index];
     }
 }
